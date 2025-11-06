43ec74e8b5acc808ddd3e219c2ee6886f55e63a1
K-jisu <wltn7star@naver.com>
2025-11-06 17:11:33 +0900

    feat: 헤더 컴포넌트 레이아웃 수정 및 아이콘 추가

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,11 +2,11 @@ import Image from 'next/image';
 import { Input } from '../ui/input';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 import { Button } from '../ui/button';
-import { Bookmark, Search } from 'lucide-react';
+import { Bookmark, Search, UserRound } from 'lucide-react';
 
 const Header = () => {
   return (
-    <div className="relative flex flex-row px-14 justify-between items-center py-3">
+    <div className="relative flex flex-row md:px-14 px-2 justify-between items-center py-3 gap-1">
       <div className="relative text-black max-w-[400px] w-full ">
         <Input className="bg-white rounded-4xl w-full" />
         <div className="absolute flex flex-row items-center justify-center gap-3 top-1/2 right-0 -translate-y-1/2">
@@ -19,23 +19,17 @@ const Header = () => {
         alt="로고"
         width={80}
         height={40}
-        className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
+        className="hidden md:absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
       />
-      <div className="relateive flex flex-row items-center">
-        <Button>
-          <Avatar className="rounded-lg">
-            <AvatarImage
-              src="https://github.com/evilrabbit.png"
-              alt="@evilrabbit"
-            />
-            <AvatarFallback>ER</AvatarFallback>
-          </Avatar>
+      <div className="relateive flex flex-row items-center gap-1 md:gap-2">
+        <Button className="hidden md:flex flex-row" variant={'white'}>
+          <UserRound fill="black" />
           <p>로그인</p>
         </Button>
-        <Button>
-          <Bookmark />
+        <Button variant={'white'}>
+          <Bookmark fill="black" />
         </Button>
-        <Button>비교하기 </Button>
+        <Button variant={'white'}>비교보기</Button>
       </div>
     </div>
   );
