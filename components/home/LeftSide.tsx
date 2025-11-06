e0e97b24bd4802e15268bc53e9eab8d813d4dc77
K-jisu <wltn7star@naver.com>
2025-11-06 17:11:07 +0900

    feat: LeftSide 컴포넌트 수정 및 예정지 추가하기 버튼 구현

--- components/home/LeftSide.tsx
+++ components/home/LeftSide.tsx
@@ -1,18 +1,9 @@
-import { Bookmark } from 'lucide-react';
+import { Bookmark, Plus } from 'lucide-react';
 import { Badge } from '../ui/badge';
 import { Button } from '../ui/button';
 import { Card, CardContent, CardFooter, CardHeader } from '../ui/card';
 import Image from 'next/image';
 import Link from 'next/link';
-
-export const FILTERBUTTON = [
-  '위치',
-  '사업성격',
-  '사업단계',
-  '시세',
-  '권리자수',
-  '신축세대수',
-];
 
 export const ITEM = [
   {
@@ -26,7 +17,7 @@ export const ITEM = [
       premium: 0,
     },
     nick_name: '닉네임닉네01099990000',
-    map: '',
+    map: '/temp_map.png',
     project_type: '재건축',
     average_land_share: 26.19,
     new_households: 2548,
@@ -44,6 +35,7 @@ export const ITEM = [
       reconstruction_area_m2: 48837.5,
       buildings: 3,
       planned_buildings: 20,
+      etc: '',
     },
     area_distribution: {
       under_60m2: 83,
@@ -65,17 +57,8 @@ export const ITEM = [
 const LeftSide = () => {
   return (
     <div className="flex flex-col max-w-[700px] w-full gap-4">
-      {/* 필터링 */}
-      <div className="flex flex-row overflow-x-scroll">
-        {FILTERBUTTON.map((item) => (
-          <Button key={item} className="flex-1" variant={'ghost'}>
-            <p className="text-2xl font-bold">{item}</p>
-          </Button>
-        ))}
-        <Button>리셋</Button>
-      </div>
       {/* 목록 */}
-      <div className="flex flex-col gap-2 px-[120px]">
+      <div className="flex flex-col gap-2 md:px-[120px] px-2">
         {ITEM.map((item, index) => (
           <Link key={index} href={`/${index}`}>
             <Card className="relative flex flex-col overflow-hidden bg-transparent p-0 rounded-4xl">
@@ -86,7 +69,7 @@ const LeftSide = () => {
                 className="object-cover -z-10"
               />
               <CardHeader className="relative flex flex-row p-0 justify-between pt-4 px-5">
-                <div className="pointer-events-none absolute inset-0 backdrop-blur-xs bg-black/7 -z-10" />
+                <div className="pointer-events-none absolute inset-0 backdrop-blur-xs -z-10" />
                 <div className="flex flex-col gap-2 ">
                   <p className="text-sm font-semibold">{item.address}</p>
                   <h3 className="text-[18px] font-bold truncate w-[200px]">
@@ -99,12 +82,22 @@ const LeftSide = () => {
               </CardHeader>
               <CardContent className="flex flex-row justify-between items-center">
                 <div className="bg-black text-white flex flex-col text-center text-[26px] rounded-4xl py-4 px-2">
-                  <p>{item.price_range.max}</p>
-                  <p>~</p>
-                  <p>{item.price_range.min}</p>
+                  <div className="flex flex-row">
+                    <p className="font-playfair">
+                      {item.price_range.max.toString().replace(/0{8}$/, '')}
+                    </p>
+                    억
+                  </div>
+                  <p className="p-0 m-0 font-playfair leading-none">~</p>
+                  <div className="flex flex-row">
+                    <p className="font-playfair">
+                      {item.price_range.min.toString().replace(/0{8}$/, '')}
+                    </p>
+                    억
+                  </div>
                 </div>
                 <div className="relative flex flex-col border-2 border-black rounded-3xl p-2 text-right gap-2 overflow-hidden h-fit">
-                  <div className="pointer-events-none absolute inset-0 backdrop-blur-xs bg-black/7 -z-10" />
+                  <div className="pointer-events-none absolute inset-0 backdrop-blur-xs -z-10" />
                   <p className="text-[20px] font-bold">{item.project_type}</p>
                   <div className="font-bold">
                     <p className="text-sm ">평균대지지분</p>
@@ -113,7 +106,7 @@ const LeftSide = () => {
                 </div>
               </CardContent>
               <CardFooter className="relative flex flex-row justify-between py-4 px-5">
-                <div className="pointer-events-none absolute inset-0 backdrop-blur-xs bg-black/7 -z-10" />
+                <div className="pointer-events-none absolute inset-0 backdrop-blur-xs -z-10" />
                 <div className="flex flex-col gap-2 font-bold">
                   <p className="text-[18px]">{item.new_households} 신축세대</p>
                   <p>임대 {item.rental_households}</p>
@@ -130,6 +123,28 @@ const LeftSide = () => {
             </Card>
           </Link>
         ))}
+      </div>
+      <div className="md:hidden relative flex flex-col flex-1 min-h-[650px] mt-10 ">
+        <Image
+          src={'/faces.png'}
+          width={400}
+          height={300}
+          alt="얼굴 아이콘들"
+          className="absolute bottom-0 left-0 right-0 w-full object-contain -z-10"
+        />
+
+        <div>
+          <p className="text-white whitespace-pre-line break-keep text-center text-[28px] font-bold">
+            {`찾는 사업장이 없나요? \n예정지 인가요? \n사업장을 추가하여 \n다른사람들에게 알려주세요`}
+          </p>
+        </div>
+
+        <Button
+          className="absolute mx-auto bottom-9 left-1/2 -translate-x-1/2 text-[25px] font-semibold rounded-full"
+          size={'lg'}
+        >
+          <Plus /> 예정지 추가하기
+        </Button>
       </div>
     </div>
   );
