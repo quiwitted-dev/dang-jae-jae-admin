47fe2203c8f25a4ca1208d0f991c58535883a3be
K-jisu <wltn7star@naver.com>
2025-11-06 17:09:55 +0900

    feat: 필터링 버튼 밖으로 빼내기

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,53 @@
-import LeftSide from '@/components/home/LeftSide';
+import LeftSide, { ITEM } from '@/components/home/LeftSide';
 import RightSide from '@/components/home/RightSide';
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 
-export default function Home() {
+export const FILTER_BUTTON = [
+  '위치',
+  '사업성격',
+  '사업단계',
+  '시세',
+  '권리자수',
+  '신축세대수',
+];
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ index?: string }>;
+}) {
+  const { index } = await searchParams;
+  const map = (index && ITEM[Number(index)].map) || '';
+
   return (
-    <main className="flex flex-row">
-      <LeftSide />
-      <RightSide />
+    <main>
+      {/* 필터링 */}
+      <div className="flex">
+        <div className="flex flex-row py-4 overflow-x-auto md:overflow-auto">
+          {FILTER_BUTTON.map((item) => (
+            <Button key={item} className="flex-1" variant={'ghost'}>
+              <p className="text-2xl font-bold">{item}</p>
+            </Button>
+          ))}
+          <Button>리셋</Button>
+        </div>
+      </div>
+      <div className="flex flex-row">
+        <LeftSide />
+        {index && (
+          <div className="flex-1 min-h-dvh">
+            <Image
+              src={map}
+              width={800}
+              height={1000}
+              alt="지도"
+              className="w-full h-full object-cover"
+            />
+          </div>
+        )}
+        {!index && <RightSide />}
+      </div>
     </main>
   );
 }
