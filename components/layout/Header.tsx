4c8db6a180e2d6e001dd6c93b7873fbee5ce0394
K-jisu <wltn7star@naver.com>
2025-11-10 16:13:43 +0900

    feat : 헤더에 모달 open 연결

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,10 +1,17 @@
+'use client';
 import Image from 'next/image';
 import { Input } from '../ui/input';
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 import { Button } from '../ui/button';
 import { Bookmark, Search, UserRound } from 'lucide-react';
+import useStore from '@/store/useStore';
 
 const Header = () => {
+  const { isOpen, toggleOpen } = useStore();
+
+  const handleSigninToggle = () => {
+    toggleOpen();
+  };
+
   return (
     <div className="relative flex flex-row md:px-14 px-2 justify-between items-center py-3 gap-1">
       <div className="relative text-black max-w-[400px] w-full ">
@@ -21,8 +28,12 @@ const Header = () => {
         height={40}
         className="hidden md:absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
       />
-      <div className="relateive flex flex-row items-center gap-1 md:gap-2">
-        <Button className="hidden md:flex flex-row" variant={'white'}>
+      <div className="relative flex flex-row items-center gap-1 md:gap-2">
+        <Button
+          className="hidden md:flex flex-row"
+          variant={'white'}
+          onClick={handleSigninToggle}
+        >
           <UserRound fill="black" />
           <p>로그인</p>
         </Button>
