038bcd82204717931fa60955ab3cb426d8c26eb6
K-jisu <wltn7star@naver.com>
2025-11-11 23:07:02 +0900

    feat :  스토어에 탭들 추가

--- store/useStore.tsx
+++ store/useStore.tsx
@@ -3,11 +3,25 @@ import { create } from 'zustand';
 interface StoreState {
   isOpen: boolean;
   toggleOpen: () => void;
+
+  myPageTab: 'message' | 'assetManagement' | 'settings' | 'none';
+  setMyPageTab: (tab: 'message' | 'assetManagement' | 'settings') => void;
+
+  settingsTab: 'privacy' | 'policies' | 'analysis' | 'service';
+  setSettingsTab: (
+    tab: 'privacy' | 'policies' | 'analysis' | 'service'
+  ) => void;
 }
 
 const useStore = create<StoreState>((set) => ({
   isOpen: false,
   toggleOpen: () => set((state) => ({ isOpen: !state.isOpen })),
+
+  myPageTab: 'none',
+  setMyPageTab: (tab) => set({ myPageTab: tab }),
+
+  settingsTab: 'privacy',
+  setSettingsTab: (tab) => set({ settingsTab: tab }),
 }));
 
 export default useStore;
