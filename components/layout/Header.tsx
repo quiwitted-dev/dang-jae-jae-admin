a7ffe7bf7a30539a5699c7d0627d18ceaccc1c41
K-jisu <wltn7star@naver.com>
2025-11-13 15:28:51 +0900

    feat: Header 컴포넌트에 모바일 및 데스크탑 레이아웃 추가 및 라우팅 기능 개선

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,50 +2,113 @@
 import Image from 'next/image';
 import { Input } from '../ui/input';
 import { Button } from '../ui/button';
-import { Bookmark, Search, UserRound } from 'lucide-react';
+import { Bookmark, ChevronLeft, Home, Search, UserRound } from 'lucide-react';
 import useStore from '@/store/useStore';
-import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
 
 const Header = () => {
-  const { isOpen, toggleOpen } = useStore();
+  const isOpen = useStore((state) => state.isOpen);
+  const toggleOpen = useStore((state) => state.toggleOpen);
+  const clear = useStore((state) => state.clear);
+  const myPageTab = useStore((state) => state.myPageTab);
+  const settingsTab = useStore((state) => state.settingsTab);
+  const pathname = usePathname();
+
+  const router = useRouter();
 
   const handleSigninToggle = () => {
     toggleOpen();
   };
 
+  const handleLink = (link: string) => {
+    router.push(link);
+    clear();
+  };
+
+  const handleBack = () => {
+    router.back();
+  };
+
   return (
-    <div className="relative flex flex-row md:px-14 px-2 justify-between items-center py-3 gap-1">
-      <div className="relative text-black max-w-[400px] w-full ">
-        <Input className="bg-white rounded-4xl w-full" />
-        <div className="absolute flex flex-row items-center justify-center gap-3 top-1/2 right-0 -translate-y-1/2">
-          검색
-          <Search />
+    <>
+      {/* 데스크탑 */}
+      <div className="hidden lg:relative lg:flex flex-row md:px-14 px-2 justify-between items-center py-3 gap-1">
+        <div className="relative text-black max-w-[400px] w-full ">
+          <Input className="bg-white rounded-4xl w-full" />
+          <div className="absolute flex flex-row items-center justify-center gap-3 top-1/2 right-0 -translate-y-1/2">
+            검색
+            <Search />
+          </div>
         </div>
-      </div>
-      <Link href={'/'}>
         <Image
           src={'/logo.png'}
           alt="로고"
           width={80}
           height={40}
-          className="hidden md:block md:absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
+          onClick={() => handleLink('/')}
+          className="hidden lg:block lg:absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 hover:cursor-pointer"
         />
-      </Link>
-      <div className="relative flex flex-row items-center gap-1 md:gap-2">
-        <Button
-          className="hidden md:flex flex-row"
-          variant={'white'}
-          onClick={handleSigninToggle}
-        >
-          <UserRound fill="black" />
-          <p>로그인</p>
-        </Button>
-        <Button variant={'white'}>
-          <Bookmark fill="black" />
-        </Button>
-        <Button variant={'white'}>비교보기</Button>
+        <div className="relative flex flex-row items-center gap-1 md:gap-2">
+          <p onClick={() => handleLink('/my')}>마이페이지</p>
+          <Button
+            className="hidden md:flex flex-row"
+            variant={'white'}
+            onClick={handleSigninToggle}
+          >
+            <UserRound fill="black" />
+            <p>로그인</p>
+          </Button>
+          <Button variant={'white'}>
+            <Bookmark fill="black" />
+          </Button>
+          <Button variant={'white'}>비교보기</Button>
+        </div>
       </div>
-    </div>
+      {/* 모바일 */}
+      <div className="lg:hidden">
+        {pathname === '/' && (
+          <div className="relative flex flex-row px-2 justify-between items-center py-3 gap-1">
+            <div className="relative text-black max-w-[400px] w-full ">
+              <Input className="bg-white rounded-4xl w-full" />
+              <div className="absolute flex flex-row items-center justify-center gap-3 top-1/2 right-0 -translate-y-1/2">
+                검색
+                <Search />
+              </div>
+            </div>
+            <div className="relative flex flex-row items-center gap-1 md:gap-2">
+              <p onClick={() => handleLink('/my')}>마이페이지</p>
+              <Button
+                className="hidden md:flex flex-row"
+                variant={'white'}
+                onClick={handleSigninToggle}
+              >
+                <UserRound fill="black" />
+                <p>로그인</p>
+              </Button>
+              <Button variant={'white'}>
+                <Bookmark fill="black" />
+              </Button>
+              <Button variant={'white'}>비교보기</Button>
+            </div>
+          </div>
+        )}
+
+        {pathname === '/my' && (
+          <div className="relative flex flex-row px-2 justify-between items-center py-3 gap-1 bg-[#212138]">
+            <div className="flex flex-row items-center justify-center gap-7">
+              <ChevronLeft onClick={handleBack} />
+              <Home onClick={() => handleLink('/')} />
+            </div>
+            <div className="relative flex flex-row items-center gap-1">
+              <Button variant={'white'}>
+                <Bookmark fill="black" />
+              </Button>
+              <Button variant={'white'}>비교보기</Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
