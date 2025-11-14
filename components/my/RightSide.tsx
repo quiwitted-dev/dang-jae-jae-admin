d772b0b425ee2a2fb1fd4deffa4e1e20cf75d4b9
K-jisu <wltn7star@naver.com>
2025-11-14 18:28:14 +0900

    feat: RightSide 컴포넌트에 설정 내비게이션 추가 및 북마크 관련 주석 추가

--- components/my/RightSide.tsx
+++ components/my/RightSide.tsx
@@ -6,6 +6,7 @@ import ProjectCard from '../common/ProjectCard';
 import Link from 'next/link';
 import SettingsNavigation from './SettingsNavigation';
 import { getSettingsContent } from '@/lib/getSettingsContent';
+import { getMainTitle } from '@/services/api';
 
 const RightSide = () => {
   const myPageTab = useStore((state) => state.myPageTab);
@@ -35,6 +36,7 @@ const RightSide = () => {
         </div>
       </div>
 
+      {/* Todo : 북마크는 max 3개 */}
       {myPageTab === 'none' && (
         <div className="grid 3xl:grid-cols-3 2xl:grid-cols-2 grid-cols-1 gap-4 mb-16 lg:mb-0">
           {ITEM.map((item, index) => (
