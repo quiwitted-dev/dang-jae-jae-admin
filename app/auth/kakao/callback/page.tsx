87934534b729373f6b8630a3b3d1296f5de652c1
K-jisu <wltn7star@naver.com>
2025-11-19 00:43:27 +0900

    feat: 카카오 로그인 콜백에서 사용자 상태 관리 추가 및 API 경로 수정

--- app/auth/kakao/callback/page.tsx
+++ app/auth/kakao/callback/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { LoginKakao } from '@/services/api';
+import { LoginKakao } from '@/services/auth.api';
+import useAuthStore from '@/store/useAuthStore';
 import { useSearchParams, useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
@@ -8,6 +9,8 @@ export default function KakaoCallbackPage() {
   const searchParams = useSearchParams();
   const code = searchParams.get('code');
   const router = useRouter();
+  const setIsLogin = useAuthStore((state) => state.setIsLogin);
+  const setUser = useAuthStore((state) => state.setUser);
 
   useEffect(() => {
     if (!code) return;
@@ -16,8 +19,11 @@ export default function KakaoCallbackPage() {
       try {
         const res = await LoginKakao(code);
         console.log(res);
+        const { id, nickname: name } = res.user;
+        setIsLogin(true);
+        setUser({ id, name });
 
-        // 로그인 성공 처리
+        // Todo : 이전에 로그인 했던 위치 기억해서 거기로 reidrect
         router.replace('/');
       } catch (error) {
         console.error('카카오 로그인 실패:', error);
