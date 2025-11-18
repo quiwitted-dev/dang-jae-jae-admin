b3c2cbe8ad65491dbb800637dadc32f4e369754d
K-jisu <wltn7star@naver.com>
2025-11-19 00:44:25 +0900

    feat: 승인된 사업 예정지 목록 및 신청 상세 조회 API 추가

---
+++ services/submission.api.ts
@@ -0,0 +1,52 @@
+import { ApprovedSubmissionList } from '@/types/type';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+// 승인된 사업 예정지 목록 조회
+export const getApprovedBusiness =
+  async (): Promise<ApprovedSubmissionList> => {
+    try {
+      const res = await fetch(`${API_URL}/api/submission/approved`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        cache: 'no-cache',
+      });
+
+      if (!res.ok) {
+        throw new Error(`API 요청 실패: ${res.status} ${res.statusText}`);
+      }
+
+      const data = await res.json();
+
+      return data;
+    } catch (error) {
+      console.error('getApprovedBusiness 에러:', error);
+      // 에러 시 기본값 반환
+      return {
+        success: false,
+        submissions: [],
+        total: 0,
+      };
+    }
+  };
+
+// 사업 예정지 신청 상세 조회
+export const getSubmissionDetail = async (id: string) => {
+  try {
+    const res = await fetch(`${API_URL}/api/submisson/${id}`, {
+      method: 'GET',
+    });
+    console.log(res);
+
+    if (!res.ok) {
+      throw new Error('사업 예정지 신청 상세 조회 실패');
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error('getSubmissionDetail 에러 : ', error);
+  }
+};
