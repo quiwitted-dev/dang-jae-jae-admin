f8ee7e9a9c18ad43404c145e2e9c8d00d36d208d
K-jisu <wltn7star@naver.com>
2025-11-19 00:44:04 +0900

    refactor: API 경로 수정 및 로그인, 프로젝트 카드 컴포넌트 개선

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,4 @@
-import { ApprovedSubmissionList, MainTitleResponse } from '@/types/type';
-import { redirect } from 'next/navigation';
+import { MainTitleResponse } from '@/types/type';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -33,81 +32,5 @@ export const getMainTitle = async (): Promise<MainTitleResponse> => {
         updatedAt: '',
       },
     };
-  }
-};
-
-export const getApprovedBusiness =
-  async (): Promise<ApprovedSubmissionList> => {
-    try {
-      const res = await fetch(`${API_URL}/api/submission/approved`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        cache: 'no-cache',
-      });
-
-      if (!res.ok) {
-        throw new Error(`API 요청 실패: ${res.status} ${res.statusText}`);
-      }
-
-      const data = await res.json();
-
-      return data;
-    } catch (error) {
-      console.error('getApprovedBusiness 에러:', error);
-      // 에러 시 기본값 반환
-      return {
-        success: false,
-        submissions: [],
-        total: 0,
-      };
-    }
-  };
-
-export const permissionKakao = async () => {
-  redirect(`${API_URL}/api/auth/kakao`);
-};
-
-export const LoginKakao = async (code: string) => {
-  const res = await fetch(`/api/auth/kakao/callback`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ code }),
-  });
-
-  if (!res.ok) {
-    const message = await res.text();
-    throw new Error(message || '카카오 로그인에 실패했습니다.');
-  }
-
-  const data = await res.json();
-
-  return data;
-};
-
-export const logout = async () => {
-  try {
-    const res = await fetch(`/api/auth/logout`, { method: 'POST' });
-    if (!res.ok) {
-      throw new Error('로그아웃 실패');
-    }
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.error('logout 에러 : ', error);
-  }
-};
-
-export const getSubmissionDetail = async () => {
-  try {
-    const res = await fetch(`${API_URL}/api/submisson`, { method: 'POST' });
-    if (!res.ok) {
-      throw new Error('로그아웃 실패');
-    }
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.error('getSubmissionDetail 에러 : ', error);
   }
 };
