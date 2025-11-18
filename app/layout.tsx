4e8a26a3fd3f5acb43bde399aaade670f54a9fdd
K-jisu <wltn7star@naver.com>
2025-11-19 00:34:51 +0900

    feat: 사용자 로그인 상태에 따라 헤더 컴포넌트에 isLoggedIn 속성 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Playfair_Display } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/layout/Header';
 import ModalProvider from '@/components/providers/ModalProviders';
+import { cookies } from 'next/headers';
 
 // Playfair Display Google 폰트 설정
 const playfairDisplay = Playfair_Display({
@@ -16,19 +17,27 @@ export const metadata: Metadata = {
   description: '당재재',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = cookies();
+  const accessToken = (await cookieStore).get('userAccessToken');
+
+  const isLoggedIn = !!accessToken;
   return (
     <html lang="ko">
       <body
         className={`${playfairDisplay.variable} antialiased bg-black text-white font-sans`}
       >
-        <Header />
+        <Header isLoggedIn={isLoggedIn} />
         {children}
         <ModalProvider />
+        <script
+          type="text/javascript"
+          src="//dapi.kakao.com/v2/maps/sdk.js?appkey=6d905d8b087267b63d28b411765e6994"
+        ></script>
       </body>
     </html>
   );
