830fb86110f7339756d30450f7c458a757b66dfa
K-jisu <wltn7star@naver.com>
2025-11-19 14:03:22 +0900

    feat : 북마크한 상태에 따라 아이콘 변경

--- components/common/Bookmark.tsx
+++ components/common/Bookmark.tsx
@@ -4,10 +4,15 @@ import { deleteBookmark, postBookmark } from '@/services/bookmark.api';
 import { BookmarkIcon } from 'lucide-react';
 import { MouseEvent, useState } from 'react';
 
-const Bookmark = ({ id }: { id: string }) => {
-  const [loading, setLoading] = useState(false);
-  const [isMark, setIsMark] = useState(false);
+type BookMarkProps = {
+  id: string;
+  isFavorite: boolean;
+};
 
+const Bookmark = ({ id, isFavorite }: BookMarkProps) => {
+  const [loading, setLoading] = useState(false);
+
+  // Todo : 북마크 삭제 요청
   const handleToggleBookmark = async (id: string, e: MouseEvent) => {
     e.stopPropagation();
     e.stopPropagation();
@@ -17,14 +22,12 @@ const Bookmark = ({ id }: { id: string }) => {
     setLoading(true);
 
     try {
-      if (!isMark) {
+      if (!isFavorite) {
         const data = await postBookmark(id);
         if (!data) throw new Error('등록 실패');
-        setIsMark(true);
       } else {
         const data = await deleteBookmark(id);
         if (!data) throw new Error('삭제 실패');
-        setIsMark(false);
       }
     } catch (err) {
       console.error(err);
@@ -36,8 +39,9 @@ const Bookmark = ({ id }: { id: string }) => {
 
   return (
     <>
-      {isMark ? (
+      {isFavorite ? (
         <BookmarkIcon
+          fill="black"
           size={16}
           onClick={(e) => {
             handleToggleBookmark(id, e);
