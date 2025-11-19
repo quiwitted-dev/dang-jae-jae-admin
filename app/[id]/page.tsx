bc73e61c253273bfd7e1e55f0156f46c5ac3eb37
K-jisu <wltn7star@naver.com>
2025-11-19 15:01:18 +0900

    working : 공공데이터 상세 조회 설정중

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,23 +1,28 @@
 import DetailRightSide from '@/components/home/DetailRightSide';
 import DetailSideBar from '@/components/home/DetailSideBar';
-import { getSubmissionDetail } from '@/services/submission.api';
+import { getSubmissionPublicDetail } from '@/services/submission.api';
 
 const Detailpage = async ({
   params,
   searchParams,
 }: {
   params: Promise<{ id: string }>;
-  searchParams: Promise<{ index?: string }>;
+  searchParams: Promise<{ source?: string }>;
 }) => {
   const { id } = await params;
-  const { index } = await searchParams;
-  const data = await getSubmissionDetail(id);
+  const { source } = await searchParams;
+  try {
+    const publicData = await getSubmissionPublicDetail(id);
+    console.log(publicData);
+  } catch (error) {
+    console.error('공공데이터 상세 조회 실패');
+  }
   // const map = ITEM[Number(id)].map;
 
   return (
     <div className="flex md:flex-row flex-col">
       <div className="order-2 md:order-1">
-        <DetailSideBar data={data} index={index ? Number(index) : Number(id)} />
+        {/* <DetailSideBar data={data} index={index ? Number(index) : Number(id)} /> */}
       </div>
       <div className="flex-1 md:min-h-dvh order-1 md:order-2">
         {/* <DetailRightSide map={map} /> */}
