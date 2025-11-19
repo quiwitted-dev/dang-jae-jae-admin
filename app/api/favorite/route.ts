8fd13415b25c4578ef8a834eb7bc26f2967f9e48
K-jisu <wltn7star@naver.com>
2025-11-19 14:02:02 +0900

    feat : 북마크 삭제, 조회 라우터 설정

--- app/api/favorite/route.ts
+++ app/api/favorite/route.ts
@@ -2,6 +2,42 @@ import { NextResponse } from 'next/server';
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
+export async function GET(request: NextResponse) {
+  try {
+    const cookie = request.headers.get('cookie') || '';
+
+    const backendRes = await fetch(`${API_URL}/api/favorite`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        cookie,
+      },
+    });
+
+    // 백엔드 응답이 실패한 경우
+    if (!backendRes.ok) {
+      const message = await backendRes.text();
+      return NextResponse.json(
+        { success: false, message: message || 'favorite API failed' },
+        { status: backendRes.status }
+      );
+    }
+
+    const data = await backendRes.json();
+
+    return NextResponse.json(
+      { success: true, data },
+      { status: backendRes.status }
+    );
+  } catch (error: any) {
+    console.error('POST /api/favorite error:', error);
+    return NextResponse.json(
+      { success: false, message: 'Internal server error' },
+      { status: 500 }
+    );
+  }
+}
+
 export async function POST(request: NextResponse) {
   try {
     const cookie = request.headers.get('cookie') || '';
@@ -14,6 +50,43 @@ export async function POST(request: NextResponse) {
         cookie,
       },
       body: JSON.stringify({ referenceId, dataType }),
+    });
+
+    // 백엔드 응답이 실패한 경우
+    if (!backendRes.ok) {
+      const message = await backendRes.text();
+      return NextResponse.json(
+        { success: false, message: message || 'favorite API failed' },
+        { status: backendRes.status }
+      );
+    }
+
+    const data = await backendRes.json();
+
+    return NextResponse.json(
+      { success: true, data },
+      { status: backendRes.status }
+    );
+  } catch (error: any) {
+    console.error('POST /api/favorite error:', error);
+    return NextResponse.json(
+      { success: false, message: 'Internal server error' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function DELETE(request: NextResponse) {
+  try {
+    const cookie = request.headers.get('cookie') || '';
+    const { id } = await request.json();
+
+    const backendRes = await fetch(`${API_URL}/api/favorite/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        cookie,
+      },
     });
 
     // 백엔드 응답이 실패한 경우
