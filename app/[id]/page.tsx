717168ad648bf4ae7d34e1566df1f804755fee69
K-jisu <wltn7star@naver.com>
2025-11-19 17:16:15 +0900

    feat : 서울 경기에 따라 공공데이터 분기 설정

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,6 +1,7 @@
-import DetailRightSide from '@/components/home/DetailRightSide';
-import DetailSideBar from '@/components/home/DetailSideBar';
+import GyeonggiSideBar from '@/components/home/GyeonggiSideBar';
+import SeoulSideBar from '@/components/home/SeoulSideBar';
 import { getSubmissionPublicDetail } from '@/services/submission.api';
+import { SubmissionPublicDetail } from '@/types/submission.type';
 
 const Detailpage = async ({
   params,
@@ -10,19 +11,26 @@ const Detailpage = async ({
   searchParams: Promise<{ source?: string }>;
 }) => {
   const { id } = await params;
-  const { source } = await searchParams;
+  let publicData: SubmissionPublicDetail | null = null;
   try {
-    const publicData = await getSubmissionPublicDetail(id);
-    console.log(publicData);
+    publicData = await getSubmissionPublicDetail(id);
   } catch (error) {
-    console.error('공공데이터 상세 조회 실패');
+    console.error('공공데이터 상세 조회 실패', error);
   }
-  // const map = ITEM[Number(id)].map;
+
+  if (!publicData) {
+    return <div>데이터를 불러올 수 없습니다.</div>;
+  }
 
   return (
     <div className="flex md:flex-row flex-col">
       <div className="order-2 md:order-1">
-        {/* <DetailSideBar data={data} index={index ? Number(index) : Number(id)} /> */}
+        {publicData.dataSource === 'SEOUL' && (
+          <SeoulSideBar publicData={publicData} />
+        )}
+        {publicData.dataSource === 'GYEONGGI' && (
+          <GyeonggiSideBar publicData={publicData} />
+        )}
       </div>
       <div className="flex-1 md:min-h-dvh order-1 md:order-2">
         {/* <DetailRightSide map={map} /> */}
