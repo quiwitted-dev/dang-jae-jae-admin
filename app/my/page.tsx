2a6e6b5b40d3cc2417eb4e6323f7006fabefe809
K-jisu <wltn7star@naver.com>
2025-11-20 16:45:30 +0900

    feat :  서버컴포넌트에서 유저 정보 가져오기

--- app/my/page.tsx
+++ app/my/page.tsx
@@ -1,12 +1,12 @@
 import LeftSide from '@/components/my/LeftSide';
 import RightSide from '@/components/my/RightSide';
+import { getUser } from '@/services/user.api.server';
+import { redirect } from 'next/navigation';
 
-export const user = {
-  name: '카카오톡아이이이이디',
-  userId: 'Fheoiid80',
-};
+const MyPage = async () => {
+  const user = await getUser();
 
-const MyPage = () => {
+  if (!user) return redirect('/');
   return (
     <div className="flex lg:flex-row flex-col bg-[#212138] min-h-dvh lg:px-0 px-2">
       <div className="lg:max-w-[600px] lg:md:w-[600px]">
