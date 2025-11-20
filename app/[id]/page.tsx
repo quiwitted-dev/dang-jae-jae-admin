e798af590cdcd395d03019ba4da0735cf5ca8626
K-jisu <wltn7star@naver.com>
2025-11-20 23:43:16 +0900

    feat : 공공데이터, 유저데이터 분기 설정

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,40 +1,43 @@
-import GyeonggiSideBar from '@/components/home/GyeonggiSideBar';
-import SeoulSideBar from '@/components/home/SeoulSideBar';
-import { getSubmissionPublicDetail } from '@/services/submission.api';
-import { SubmissionPublicDetail } from '@/types/submission.type';
+import PublicData from '@/components/home/[id]/PublicData';
+import SubmissionData from '@/components/home/[id]/SubmissionData';
+import {
+  getSubmissionPublicDetail,
+  getSubmissionUserDetail,
+} from '@/services/submission.api';
+import {
+  SubmissionPublicDetail,
+  SubmissionUserDetail,
+} from '@/types/submission.type';
 
 const Detailpage = async ({
   params,
   searchParams,
 }: {
   params: Promise<{ id: string }>;
-  searchParams: Promise<{ source?: string }>;
+  searchParams: Promise<{ type: 'PUBLIC_DATA' | 'SUBMISSION' }>;
 }) => {
   const { id } = await params;
-  let publicData: SubmissionPublicDetail | null = null;
-  try {
-    publicData = await getSubmissionPublicDetail(id);
-  } catch (error) {
-    console.error('공공데이터 상세 조회 실패', error);
+  const dataType = (await searchParams).type;
+
+  const fetchData = {
+    PUBLIC_DATA: getSubmissionPublicDetail,
+    SUBMISSION: getSubmissionUserDetail,
+  };
+
+  if (!dataType) {
+    return <div>잘못된 접근입니다.</div>;
   }
 
-  if (!publicData) {
-    return <div>데이터를 불러올 수 없습니다.</div>;
-  }
+  const data = await fetchData[dataType]?.(id);
 
   return (
     <div className="flex md:flex-row flex-col">
-      <div className="order-2 md:order-1">
-        {publicData.dataSource === 'SEOUL' && (
-          <SeoulSideBar publicData={publicData} />
-        )}
-        {publicData.dataSource === 'GYEONGGI' && (
-          <GyeonggiSideBar publicData={publicData} />
-        )}
-      </div>
-      <div className="flex-1 md:min-h-dvh order-1 md:order-2">
-        {/* <DetailRightSide map={map} /> */}
-      </div>
+      {dataType === 'PUBLIC_DATA' && (
+        <PublicData publicData={data as SubmissionPublicDetail} />
+      )}
+      {dataType === 'SUBMISSION' && (
+        <SubmissionData submissionData={data as SubmissionUserDetail} />
+      )}
     </div>
   );
 };
