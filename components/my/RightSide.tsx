ab443bcadddec9c496dbc1a3dded532841822162
K-jisu <wltn7star@naver.com>
2025-11-21 05:11:18 +0900

    feat : 북마크

--- components/my/RightSide.tsx
+++ components/my/RightSide.tsx
@@ -5,11 +5,23 @@ import ProjectCard from '../common/ProjectCard';
 import Link from 'next/link';
 import SettingsNavigation from './SettingsNavigation';
 import { getSettingsContent } from '@/lib/getSettingsContent';
+import { useEffect, useState } from 'react';
+import { getBookmark } from '@/services/bookmark.api';
+import { Favorite } from '@/types/favorite.type';
 
 const RightSide = () => {
   const myPageTab = useStore((state) => state.myPageTab);
   const settingsTab = useStore((state) => state.settingsTab);
-  const item = [];
+  const [myBookmark, setMyBookmark] = useState<Favorite[]>([]);
+  // todo 북마크 가지고 다시 데이터 가져온 후 item으로 넘기기
+
+  useEffect(() => {
+    (async () => {
+      const data = await getBookmark();
+      setMyBookmark(data.favorites);
+    })();
+  }, []);
+  console.log(myBookmark);
 
   const getTabTitle = () => {
     switch (settingsTab) {
@@ -26,6 +38,8 @@ const RightSide = () => {
     }
   };
 
+  if (!myBookmark) return null;
+
   return (
     <div className="flex flex-col w-full md:px-9 md:pt-16 md:gap-20">
       <div className="flex flex-row justify-between items-center">
@@ -38,20 +52,20 @@ const RightSide = () => {
       {/* Todo : 북마크는 max 3개 */}
       {myPageTab === 'none' && (
         <div className="grid 3xl:grid-cols-3 2xl:grid-cols-2 grid-cols-1 gap-4 mb-16 lg:mb-0">
-          {item.map((item, index) => (
+          {myBookmark.map((item, index) => (
             <div
               className="flex flex-col gap-4 text-[#FAFFCE] relative"
               key={index}
             >
               <h3 className="text-4xl text-center font-medium">
-                {item.address}
+                {item.publicRenovationData.address}
               </h3>
               <Link href={`/${index}`}>
                 <div className="relative z-0 mx-auto">
-                  <ProjectCard item={item} />
+                  {/* <ProjectCard item={item} /> */}
                 </div>
               </Link>
-              <p className="text-center text-base">{item.project_name}</p>
+              {/* <p className="text-center text-base">{item.project_name}</p> */}
             </div>
           ))}
         </div>
