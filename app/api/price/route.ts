95501d3d8ec3045f15d1af48c3ac6aa0f755a899
K-jisu <wltn7star@naver.com>
2025-11-21 16:29:11 +0900

    feat : 가격 설정 요청 라우터 추가

---
+++ app/api/price/route.ts
@@ -0,0 +1,41 @@
+import { API_URL } from '@/services/api';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: NextResponse) {
+  try {
+    const cookie = request.headers.get('cookie') || '';
+    const form = await request.json();
+    console.log(form);
+
+    const backendRes = await fetch(`${API_URL}/api/price`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        cookie,
+      },
+      body: JSON.stringify(form),
+    });
+
+    // 백엔드 응답이 실패한 경우
+    if (!backendRes.ok) {
+      const message = await backendRes.text();
+      return NextResponse.json(
+        { success: false, message: message || 'favorite API failed' },
+        { status: backendRes.status }
+      );
+    }
+
+    const data = await backendRes.json();
+
+    return NextResponse.json(
+      { success: true, data },
+      { status: backendRes.status }
+    );
+  } catch (error: any) {
+    console.error('POST /api/favorite error:', error);
+    return NextResponse.json(
+      { success: false, message: 'Internal server error' },
+      { status: 500 }
+    );
+  }
+}
