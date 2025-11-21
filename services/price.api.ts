fdd9e465c8aa01d56a84210906b5a3530688e6ee
K-jisu <wltn7star@naver.com>
2025-11-21 16:27:11 +0900

    feat : 가격 설정 api 추가

---
+++ services/price.api.ts
@@ -0,0 +1,60 @@
+import { PriceType } from '@/types/price.type';
+
+type PostPriceType = {
+  referenceId: string;
+  dataType: 'PUBLIC_DATA' | 'SUBMISSION';
+  form: {
+    minPrice: string;
+    maxPrice: string;
+    minimumInitialInvestment?: string;
+    premium?: string;
+  };
+};
+
+export const postPrice = async ({
+  referenceId,
+  dataType,
+  form,
+}: PostPriceType): Promise<PriceType> => {
+  if (dataType === 'PUBLIC_DATA') {
+    const res = await fetch(`/api/price`, {
+      method: 'POST',
+      body: JSON.stringify({
+        referenceId,
+        dataType,
+        minPrice: form.minPrice,
+        maxPrice: form.maxPrice,
+        minimumInitialInvestment: form.minimumInitialInvestment,
+        premium: form.premium,
+      }),
+    });
+
+    if (!res.ok) {
+      throw new Error(`가격 입력 실패 ${res.status} ${res.statusText}`);
+    }
+
+    const data = await res.json();
+    console.log(data);
+    return data.data;
+  }
+  if (dataType === 'SUBMISSION') {
+    const res = await fetch(`/api/price`, {
+      method: 'POST',
+      body: JSON.stringify({
+        referenceId,
+        dataType,
+        minPrice: form.minPrice,
+        maxPrice: form.maxPrice,
+      }),
+    });
+
+    if (!res.ok) {
+      throw new Error(`가격 입력 실패 ${res.status} ${res.statusText}`);
+    }
+
+    const data = await res.json();
+    return data.data;
+  }
+
+  throw new Error(`지원하지 않는 dataType: ${dataType}`);
+};
