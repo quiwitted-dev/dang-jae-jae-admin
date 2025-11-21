76fcfe9b64f480c93514a89b9b984ed81dbea25e
K-jisu <wltn7star@naver.com>
2025-11-21 11:25:51 +0900

    feat : 필터링 컴포넌트 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 import LeftSide from '@/components/home/LeftSide';
 import RightSide from '@/components/home/RightSide';
+import LocationFilter from '@/components/home/LocationFilter';
+import BusinessTypeFilter from '@/components/home/BusinessTypeFilter';
+import BusinessStageFilter from '@/components/home/BusinessStageFilter';
+import PriceFilter from '@/components/home/PriceFilter';
+import OwnerCountFilter from '@/components/home/OwnerCountFilter';
+import NewUnitsFilter from '@/components/home/NewUnitsFilter';
 import { Button } from '@/components/ui/button';
 import { getApprovedBusiness } from '@/services/submission.api';
 
-export const FILTER_BUTTON = [
-  '위치',
-  '사업성격',
-  '사업단계',
-  '시세',
-  '권리자수',
-  '신축세대수',
-];
+export const FILTER_BUTTON: string[] = [];
 
 export default async function Home({
   searchParams,
@@ -27,11 +26,12 @@ export default async function Home({
       {/* 필터링 */}
       <div className="flex">
         <div className="flex flex-row py-4 overflow-x-auto md:overflow-auto">
-          {FILTER_BUTTON.map((item) => (
-            <Button key={item} className="flex-1" variant={'ghost'}>
-              <p className="text-2xl font-bold">{item}</p>
-            </Button>
-          ))}
+          <LocationFilter />
+          <BusinessTypeFilter />
+          <BusinessStageFilter />
+          <PriceFilter />
+          <OwnerCountFilter />
+          <NewUnitsFilter />
           <Button>리셋</Button>
         </div>
       </div>
