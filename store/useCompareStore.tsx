a15f8ee7a443fefa8ecc5e048bec17bb4545e865
K-jisu <wltn7star@naver.com>
2025-11-21 16:26:56 +0900

    feat : 비교하기 스토어 수정

--- store/useCompareStore.tsx
+++ store/useCompareStore.tsx
@@ -1,22 +1,27 @@
 import { create } from 'zustand';
 
+export type CompareItem = {
+  id: string;
+  dataType: 'PUBLIC' | 'SUBMISSON';
+};
+
 type CompareStore = {
-  compare: [string | null, string | null];
-  setCompare: (id: string) => void; // 첫 슬롯 비면 채우고, 아니면 두 번째 교체
+  compare: [CompareItem | null, CompareItem | null];
+  setCompare: (item: CompareItem) => void; // 첫 슬롯 비면 채우고, 아니면 두 번째 교체
   clear: () => void;
 };
 
 const useCompareStore = create<CompareStore>((set) => ({
   compare: [null, null],
-  setCompare: (id) =>
+  setCompare: (item) =>
     set(({ compare }) => {
       const [a, b] = compare;
-      if (!a) return { compare: [id, b] };
-      if (!b) return { compare: [a, id] };
+      if (!a) return { compare: [item, b] };
+      if (!b) return { compare: [a, item] };
       // 둘 다 찼으면 순환 교체
-      return { compare: [b, id] };
+      return { compare: [b, item] };
     }),
   clear: () => set({ compare: [null, null] }),
 }));
 
-export default useCompareStore
+export default useCompareStore;
