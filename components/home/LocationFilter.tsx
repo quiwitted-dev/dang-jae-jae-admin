3c242b661c98e64389c8cd84ba172b2d47896b69
K-jisu <wltn7star@naver.com>
2025-11-24 18:00:03 +0900

    feat : 초기화 추가

--- components/home/LocationFilter.tsx
+++ components/home/LocationFilter.tsx
@@ -162,6 +162,7 @@ export default function LocationFilter() {
 
   const handleReset = () => {
     setLocations([]);
+    setSelectRegion({ region: '', district: '' });
     setIsOpen(false);
     setShowDistricts('');
   };
