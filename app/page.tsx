7a8c5a9dc501d03da29f8e42a6fb053dff039a29
K-jisu <wltn7star@naver.com>
2025-11-26 18:57:50 +0900

    feat : id 파라미터 제거 및 컴포넌트 분할

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,24 @@
 import LeftSide from '@/components/home/LeftSide';
-import RightSide from '@/components/home/RightSide';
 import { getApprovedBusiness } from '@/services/submission.api';
-import MapView from '@/components/home/MapView';
+import HomeRightSide from '@/components/home/HomeRightSide';
 
 export const FILTER_BUTTON: string[] = [];
 
 export default async function Home({
   searchParams,
 }: {
-  searchParams: Promise<{ id?: string; page?: string }>;
+  searchParams: Promise<{ page?: string }>;
 }) {
   const params = await searchParams;
-  const { id, ...query } = params;
 
-  const data = await getApprovedBusiness(query);
-
-  const selected = id
-    ? data.submissions.find((submission) => submission.id === id)
-    : undefined;
-  const mapAddress = selected?.address;
+  const data = await getApprovedBusiness(params);
 
   return (
     <main>
       {/* 필터링 */}
       <div className="flex flex-row gap-5">
         <LeftSide data={data} />
-        {selected && (
-          <div className="flex-1 min-h-dvh max-h-screen">
-            <MapView address={mapAddress} />
-          </div>
-        )}
-        {!selected && <RightSide />}
+        <HomeRightSide />
       </div>
     </main>
   );
