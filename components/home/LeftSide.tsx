88e8bc647c0eb9ee390bbe985422e1f934036308
K-jisu <wltn7star@naver.com>
2025-12-05 18:36:16 +0900

    fix : 네비게이션 및 필터링 sticky 수정

--- components/home/LeftSide.tsx
+++ components/home/LeftSide.tsx
@@ -145,28 +145,32 @@ const LeftSide = ({ data }: { data: ApprovedSubmissionList }) => {
   return (
     <div className="flex flex-col relative max-w-[550px] w-full">
       <div
-        ref={filterRef}
-        className="sticky top-0 flex flex-row overflow-x-auto md:overflow-auto scrollbar-hide cursor-grab active:cursor-grabbing select-none z-30 bg-black py-2"
+        className="sticky md:top-0 top-12 z-30 bg-black"
         style={{ top: 'var(--header-offset, 0px)' }}
-        onMouseDown={handleMouseDown}
-        onMouseLeave={handleMouseLeave}
-        onMouseUp={handleMouseUp}
-        onMouseMove={handleMouseMove}
       >
-        <LocationFilter />
-        <BusinessTypeFilter />
-        <BusinessStageFilter />
-        <PriceFilter />
-        <OwnerCountFilter />
-        <NewUnitsFilter />
-        <div className="flex flex-row gap-1">
-          <Button
-            onClick={handleResetFilters}
-            className="bg-[#532f4d] text-black rounded-4xl"
-            variant={'ghost'}
-          >
-            리셋
-          </Button>
+        <div
+          ref={filterRef}
+          className="flex flex-row overflow-x-auto md:overflow-auto scrollbar-hide cursor-grab active:cursor-grabbing select-none z-30 bg-black py-2"
+          onMouseDown={handleMouseDown}
+          onMouseLeave={handleMouseLeave}
+          onMouseUp={handleMouseUp}
+          onMouseMove={handleMouseMove}
+        >
+          <LocationFilter />
+          <BusinessTypeFilter />
+          <BusinessStageFilter />
+          <PriceFilter />
+          <OwnerCountFilter />
+          <NewUnitsFilter />
+          <div className="flex flex-row gap-1">
+            <Button
+              onClick={handleResetFilters}
+              className="bg-[#532f4d] text-black rounded-4xl"
+              variant={'ghost'}
+            >
+              리셋
+            </Button>
+          </div>
         </div>
       </div>
       {/* 목록 */}
