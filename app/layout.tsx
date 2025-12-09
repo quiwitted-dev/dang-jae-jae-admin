8ea79cf01623b1d8e66d36992d55e972943772df
K-jisu <wltn7star@naver.com>
2025-12-09 21:09:10 +0900

    feat : ToastProvider 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from '@/components/layout/Header';
 import ModalProvider from '@/components/providers/ModalProviders';
 import { cookies } from 'next/headers';
 import { getUser } from '@/services/user.api.server';
+import { ToastProvider } from '@/components/providers/ToastProvider';
 
 // Playfair Display Google 폰트 설정
 const playfairDisplay = Playfair_Display({
@@ -34,6 +35,7 @@ export default async function RootLayout({
         <Header isLoggedIn={isLoggedIn} />
         <main>{children}</main>
         <ModalProvider />
+        <ToastProvider />
         <script
           type="text/javascript"
           src="//dapi.kakao.com/v2/maps/sdk.js?appkey=0dacc0c4e114487ed38c366e80cd1d4b&autoload=false&libraries=services"
