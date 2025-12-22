d72fc5fb898ebe78a94d66eac386e5c8524fb8ea
K-jisu <wltn7star@naver.com>
2025-12-22 13:44:38 +0900

    fix : 홈화면 우측 내용 화면 크기에 맞게 수정

--- components/common/InfoCard.tsx
+++ components/common/InfoCard.tsx
@@ -10,10 +10,10 @@ type InfoCardProps = {
 const InfoCard = ({ info }: { info: InfoCardProps }) => {
   return (
     <div key={info.title} className="flex flex-row">
-      <div className="relative p-0 min-w-[94px]">
+      <div className="xl:block hidden relative p-0 min-w-[94px]">
         <Image src={info.img} alt={info.title} fill className="object-cover" />
       </div>
-      <div className="py-10 px-7 flex flex-col gap-4 bg-white/4">
+      <div className="xl:py-10 xl:px-7 py-5 pr-3 flex flex-col gap-4 bg-white/4">
         <h3 className={`${info.color} text-base`}>{info.title}</h3>
         <p className="text-white whitespace-normal break-keep">{info.desc}</p>
       </div>
