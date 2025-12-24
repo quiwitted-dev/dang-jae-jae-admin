4901016951577ae759fb8977caa4092463418f0f
K-jisu <wltn7star@naver.com>
2025-12-24 13:46:06 +0900

    fix : trycatch로 서버에서 받아오는 데이터 핸들링

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import LeftSide from '@/components/home/LeftSide';
 import { getApprovedBusiness } from '@/services/submission.api';
 import HomeRightSide from '@/components/home/HomeRightSide';
+import { ApprovedSubmissionList } from '@/types/submission.type';
 
 export const FILTER_BUTTON: string[] = [];
 
@@ -10,8 +11,12 @@ export default async function Home({
   searchParams: Promise<{ page?: string }>;
 }) {
   const params = await searchParams;
-
-  const data = await getApprovedBusiness(params);
+  let data: ApprovedSubmissionList = { success: false, submissions: [] };
+  try {
+    data = await getApprovedBusiness(params);
+  } catch (error) {
+    console.error('getApprovedBusiness Failed', error);
+  }
 
   return (
     <main>
